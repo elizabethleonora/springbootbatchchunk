618d9c16e6d3b6c63e5e797ee61deef497b1acbe
E216150 <elizabeth.ngo@evry.com>
2019-06-28 15:36:45 +0200

    Initial commit

---
+++ src/main/java/com/elizabeth/entity/User.java
@@ -0,0 +1,20 @@
+package com.elizabeth.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    private Long userId;
+
+    private String name;
+
+    private BigDecimal accountBalance;
+
+}
